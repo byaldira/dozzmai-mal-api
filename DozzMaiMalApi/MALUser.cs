ca8c739182a0163980dde4407db89ce5eeef8608
Cem Küçükhasanoğlu <kucukhasanoglu.cem@gmail.com>
2016-02-17 00:26:44 +0200

    Anime Query functionality added
    MALClient.cs
    -> AnimeQuery method was added.
    -> GetAnimeFromEntry method created: This method creates a MALAnime
    object using the data given as the parameter 'element'
    -> StringToQueryParameter method added: this converts a normal string to
    one that could be used in the request .
    
    MALUser.cs
    -> A constructor that doesn't require user credentials added. This will
    also protect any developers account from being hacked, or stolen. Use
    this project as a reference in your test project, that is on a directory
    seperate from the git directory.
    -> Authenticate user has been made public
    -> setters addded to username and passworrd properties

--- DozzMaiMalApi/MALUser.cs
+++ DozzMaiMalApi/MALUser.cs
@@ -18,7 +18,24 @@ namespace DozzMaiMalApi
         private bool isAuthenticated;
 
 
+
         #region Methods
+
+        /// <summary>
+        /// Instantiates the MALUser class
+        /// </summary>
+        /// <param name="client">Web Client object that will hadle the request</param>
+        public MALUser(object client)
+        {
+            userID = null;
+            isAuthenticated = false;
+
+            // Assign verification url
+            verificationUrl = "http://myanimelist.net/api/account/verify_credentials.xml";
+
+            webClient = client as WebClient;
+        }
+
         /// <summary>
         /// Instantiates the MALUser class
         /// </summary>
@@ -43,7 +60,7 @@ namespace DozzMaiMalApi
         /// <summary>
         /// Authenticates the user using the given credentials
         /// </summary>
-        private void AuthenticateUser()
+        public void AuthenticateUser()
         {
             // Convert credentials to base 64 string
             byte[] bytes = Encoding.UTF8.GetBytes(userName + ":" + password);
@@ -66,15 +83,20 @@ namespace DozzMaiMalApi
                 throw new VerficationFailedException(msg);
             }
         }
+
         #endregion
 
+
+
         #region Properties
+
         /// <summary>
         /// Get the username of the current user
         /// </summary>
         public string UserName
         {
             get { return userName; }
+            set { userName = value; }
         }
 
 
@@ -93,7 +115,9 @@ namespace DozzMaiMalApi
         protected string Password
         {
             get { return password; }
+            set { password = value; }
         }
+
         #endregion
     }
 }
--- DozzMaiMalApi/MalClient.cs
+++ DozzMaiMalApi/MalClient.cs
@@ -19,8 +19,150 @@ namespace DozzMaiMalApi
             // Initialize web client
             webClient = new WebClient();
 
-            // Set dummy user credentials
-            user = new MALUser("cemkck", "62qypvrj6fja", webClient);
+            // Initialize user
+            user = new MALUser(webClient);
         }
+
+
+
+        #region Methods
+
+        #region -> Public Usage Methods
+
+        public IQueryable<MALAnime> AnimeQuery(string str)
+        {
+            try
+            {
+                // Generate anime query string
+                string param = StringToQueryParameter(str);
+                string queryString = "http://myanimelist.net/api/anime/search.xml?q=" + param;
+
+                string xml = webClient.DownloadString(queryString);
+
+                // Create xml document
+                var document = new XmlDocument();
+                document.LoadXml(xml);      // Read the xml data from the string
+
+                // DEBUG!!!
+                // Get the root node in the xml
+                var root = document.DocumentElement;
+
+                // Create a queryable animes object
+                var animeList = new List<MALAnime>();
+
+                foreach (var child in root)
+                {
+                    var c = child as XmlNode;
+
+                    // If the node is an entry node
+                    if (c.Name == "entry")
+                        animeList.Add(GetAnimeFromEntry(c));    // Add the anime underlying anime data to the list
+                }
+
+                // Return the queryable animes object
+                return (IQueryable<MALAnime>)animeList;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Message);
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region -> Essentials (Private)
+
+        private MALAnime GetAnimeFromEntry(object element)
+        {
+            // Convert element to xml element
+            var xmlElement = element as XmlElement;
+
+            // Create a new empty anime object
+            var anime = new MALAnime();
+
+            foreach (var item in xmlElement)
+            {
+                var c = item as XmlNode;
+
+                // DEBUG!!
+                Debug.WriteLine(c.Name + " : " + c.InnerXml);
+
+                // Fill anime data by node name
+                switch (c.Name)
+                {
+                    case "id":
+                        anime.ID = Convert.ToInt32(c.InnerXml);
+                        break;
+
+                    case "title":
+                        anime.Title = c.InnerXml;
+                        break;
+
+                    case "english":
+                        anime.English = c.InnerXml;
+                        break;
+
+                    case "sysnonyms":
+                        anime.Synonyms = c.InnerXml;
+                        break;
+
+                    case "episodes":
+                        anime.Episodes = Convert.ToInt32(c.InnerXml);
+                        break;
+
+                    case "score":
+                        anime.Score = Convert.ToDouble(c.InnerXml);
+                        break;
+
+                    case "type":
+                        anime.Type = c.InnerXml;
+                        break;
+
+                    case "status":
+                        anime.Status = c.InnerXml;
+                        break;
+
+                    case "start_date":
+                        anime.StartDate = c.InnerXml;
+                        break;
+
+                    case "end_date":
+                        anime.EndDate = c.InnerXml;
+                        break;
+
+                    case "synopsis":
+                        anime.Synopsis = c.InnerXml;
+                        break;
+
+                    case "image":
+                        anime.ImageUrl = c.InnerXml;
+                        break;
+                }
+            }
+
+            return anime;
+        }
+
+        private string StringToQueryParameter(string param)
+        {
+            return param.Replace(' ', '+');
+        }
+
+        #endregion
+
+        #endregion
+
+
+
+        #region Properties
+
+        public MALUser User
+        {
+            get { return user; }
+        }
+
+        #endregion
     }
 }
